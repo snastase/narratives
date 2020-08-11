7e67995f1c55f32a0c55fb5581b9b0a4b9ac4b5d
Sam Nastase <sam.nastase@gmail.com>
2020-08-10 20:29:56 -0400

    Surface and volumetric spatial smoothing

---
+++ code/run_smoothing.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+from sys import argv
+from os.path import basename, exists, join
+from os import makedirs, remove
+import json
+from subprocess import run
+
+
+# Pull in variables (from Slurm submission)
+subject = f'sub-{argv[1]}'
+space = argv[2]
+width = argv[3]
+
+
+# Check that we get a reasonable space
+if 'MNI' not in space and 'fsaverage' not in space:
+    raise AssertionError("Expected either MNI or fsaverage space")
+
+
+# Create output AFNI directory if it doesn't exist
+deriv_dir = '/jukebox/hasson/snastase/narratives/derivatives'
+fmriprep_dir = join(deriv_dir, 'fmriprep', subject, 'func')
+freesurfer_dir = join(deriv_dir, 'freesurfer')
+
+
+# Assign an AFNI pipeline output directory (either -smooth or -nosmooth)
+afni_pipe = 'afni-smooth'
+afni_dir = join(deriv_dir, afni_pipe, subject, 'func')
+if not exists(afni_dir):
+    makedirs(afni_dir)
+
+if 'fsaverage' in space:
+    surf_dir = join(deriv_dir, 'freesurfer', space, 'surf')
+    surf_fns = {'L': join(surf_dir, 'lh.pial'),
+                'R': join(surf_dir, 'rh.pial')}
+
+
+# Load subject metadata to get filenames
+with open(join(base_dir, 'code', 'subject_meta.json')) as f:
+    subject_meta = json.load(f)
+
+bold_fns = subject_meta[subject]['bold'][space]['preproc']
+
+
+# Loop through BOLD images and smooth
+for bold_fn in bold_fns:
+    task = bold_fn.split('task-')[1].split('_')[0]
+
+    # Volumetric smoothing for MNI space
+    if 'MNI' in space:
+        mask_fn = join(deriv_dir, 'afni', f'tpl-{space}',
+                       f'tpl-{space}_res-{task}_desc-brain_mask.nii.gz')
+        smooth_fn = join(afni_dir, basename(bold_fn).replace(
+            'desc-preproc', f'desc-sm{width}'))
+        if not exists(smooth_fn):
+            run(f"3dBlurToFWHM -mask {mask_fn} -FWHM {width} "
+                f"-input {bold_fn} -prefix {smooth_fn} "
+                f"-blurmaster {bold_fn} -detrend -bmall", shell=True)
+
+    # Surface-based smoothing for fsaverage space
+    elif 'fsaverage' in space:
+        hemi = bold_fn.split('hemi-')[1][0]
+        smooth_fn = join(afni_dir, basename(bold_fn).replace(
+            '.func.gii', f'_desc-sm{width}.func.gii'))
+        
+        if task in ['schema', 'shapesphysical', 'shapessocial']:
+            sigma = .6
+            rerun = True
+        elif task in ['black', 'forgot', 'bronx', 'piemanpni']:
+            sigma = .55
+            rerun = True
+        else:
+            sigma = .5
+            rerun = False
+
+        if rerun:
+            run(f"SurfSmooth -input {bold_fn} -target_fwhm {width} "
+                f"-i_fs {surf_fns[hemi]} -output {smooth_fn} "
+                f"-blurmaster {bold_fn} -detrend_master "
+                f"-met HEAT_07 -bmall -sigma {sigma} -overwrite", shell=True)
+        else:
+            if not exists(smooth_fn):
+                run(f"SurfSmooth -input {bold_fn} -target_fwhm {width} "
+                    f"-i_fs {surf_fns[hemi]} -output {smooth_fn} "
+                    f"-blurmaster {bold_fn} -detrend_master "
+                    f"-met HEAT_07 -bmall -sigma {sigma} -overwrite", shell=True)
---
+++ code/slurm_smoothing.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Run within BIDS code/ directory:
+# sbatch slurm_smoothing.sh
+
+# Set partition
+#SBATCH --partition=all
+
+# How long is job (in minutes)?
+#SBATCH --time=480
+
+# How much memory to allocate (in MB)?
+#SBATCH --cpus-per-task=2 --mem-per-cpu=10000
+
+# Name of jobs?
+#SBATCH --job-name=smoothing
+
+# Where to output log files?
+#SBATCH --output='../derivatives/logs/smoothing-rerun5to6-%A_%a.log'
+
+# Number jobs to run in parallel, pass index as subject ID
+#SBATCH --array=1-345
+
+# Remove modules because Singularity shouldn't need them
+echo "Purging modules"
+module purge
+
+# Load AFNI module
+echo "Loading AFNI module afni/2019.10.09"
+module load afni/2019.10.09
+afni --version
+
+# Print job submission info
+echo "Slurm job ID: " $SLURM_JOB_ID
+echo "Slurm array task ID: " $SLURM_ARRAY_TASK_ID
+date
+
+# Set subject ID based on array index
+printf -v subj "%03d" $SLURM_ARRAY_TASK_ID
+
+# Run fMRIPrep script with participant argument
+echo "Running spatial smoothing on sub-$subj"
+
+./run_smoothing.py $subj MNI152NLin2009cAsym 6
+./run_smoothing.py $subj fsaverage6 6
+
+echo "Finished spatially smoothing sub-$subj"
+date

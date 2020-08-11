20c9fe1fd1dfb9166425d40eebc3b2e85d852999
Sam Nastase <sam.nastase@gmail.com>
2020-08-11 15:01:05 -0400

    Update metadata w/ event onset metadata for trimming

--- code/compile_metadata.py
+++ code/compile_metadata.py
@@ -3,12 +3,12 @@ from glob import glob
 import json
 from natsort import natsorted
 import pandas as pd
+import numpy as np
 
 
 base_dir = '/jukebox/hasson/snastase/narratives'
 preproc_dir = join(base_dir, 'derivatives', 'fmriprep')
-afni_dir = join(base_dir, 'derivatives', 'afni')
-
+afni_dir = join(base_dir, 'derivatives', 'afni-smooth')
 
 
 # Get our subject list from the BIDS directory
@@ -16,11 +16,13 @@ n_subjects = 345
 subjects = natsorted([p.split('/')[-2] for p in
                       glob(join(preproc_dir, 'sub-*/'))])
 
+
 # Check that we're not missing subjects
 for n, subject in zip(range(1, n_subjects + 1), subjects):
     if subject != f"sub-{n:03}":
         raise AssertionError("Found a mismatching subject: "
                              f"{subject} (expected sub-{n:03})")
+
 
 # Create a dictionary containing filenames keyed to subjects
 spaces = ['MNI152NLin2009cAsym', 'fsaverage6']
@@ -61,7 +63,7 @@ for subject in subjects:
                                                 width: smooth_fns}
 
 # Save the subject metadata dictionary
-with open(join(base_dir, 'subject_meta.json'), 'w') as f:
+with open(join(base_dir, 'code', 'subject_meta.json'), 'w') as f:
     json.dump(subject_meta, f, indent=2, sort_keys=True)
 
 
@@ -106,55 +108,59 @@ for task in tasks:
                                 desc].append(bold_fn)
 
 # Save the task metadata dictionary
-with open(join(base_dir, 'task_meta.json'), 'w') as f:
+with open(join(base_dir, 'code', 'task_meta.json'), 'w') as f:
     json.dump(task_meta, f, indent=2, sort_keys=True)
 
 
-# Set up some subjects to exclude a priori due to
-# e.g. mismatching TRs, poor behavior, noncompliance
-task_exclude = {'pieman': [],
-                'tunnel': ['sub-004', 'sub-013'],
-                'lucy': [],
-                'prettymouth': ['sub-038', 'sub-105'],
-                'milkyway': ['subj-105', 'sub-123', 'sub-038'],
-                'slumlordreach': ['sub-139'],
-                'notthefallintact': [],
-                'notthefalllongscram': [],
-                'notthefallshortscram': [],
-                'merlin': [],
-                'sherlock': [],
-                'schema': [],
-                'shapesphysical': [],
-                'shapessocial': [],
-                '21styear': [],
-                'bronx': [],
-                'pieman': [],
-                'black': [],
-                'forgot': []}
+# Get onsets and durations from events.tsv for trimming
+tr = 1.5
 
-with open(join(base_dir, 'task_exclude.json'), 'w') as f:
-    json.dump(task_exclude, f, indent=2, sort_keys=True)
+event_meta = {}
+for task in task_meta:
 
+    # Skip 'schema' task for simplicity
+    if task == 'schema':
+        continue
 
-# Compile TRs (not seconds!) to trim per task prior to ISC
-# Ultimately, we should get this in a smarter way via events.tsv
-task_trims = {'pieman': {'start': 10, 'end': 8},
-              'tunnel': {'start': 2, 'end': 15},
-              'lucy': {'start': 2, 'end': 6},
-              'prettymouth': {'start': 14, 'end': 10},
-              'milkyway': {'start': 14, 'end': 10},
-              'slumlordreach': {'start': 20, 'end': 8},
-              'notthefallintact': {'start': 2, 'end': 8},
-              'notthefalllongscram': {'start': 2, 'end': 8},
-              'notthefallshortscram': {'start': 2, 'end': 8},
-              '21styear': {'start': 14, 'end': 10},
-              'bronx': {'start': 8, 'end': 8},
-              'piemanpni': {'start': 8, 'end': 8},
-              'black': {'start': 8, 'end': 8},
-              'forgot': {'start': 8, 'end': 8}}                   
+    for subject in task_meta[task]:
+        events_fns = glob(join(base_dir, subject, 'func',
+                               f'{subject}_task-{task}_*events.tsv'))
+        for events_fn in events_fns:
+            events = pd.read_csv(events_fn, sep='\t')
+            story = events[events['trial_type'] == 'story']
 
-with open(join(base_dir, 'task_trims.json'), 'w') as f:
-    json.dump(task_trims, f, indent=2, sort_keys=True)
+            # Populate with first subject's events values
+            if task not in event_meta:
+                event_meta[task] = {}
+
+                if task == 'schema':
+                    pass
+
+                elif task == 'slumlordreach':
+                    event_meta[task]['slumlord'] = {
+                        'onset': int(np.rint(story.iloc[0]['onset'] / tr)),
+                        'duration': int(np.rint(story.iloc[0]['duration'] / tr))}
+                    event_meta[task]['reach'] = {
+                        'onset': int(np.rint(story.iloc[1]['onset'] / tr)),
+                        'duration': int(np.rint(story.iloc[1]['duration'] / tr))}
+
+                else:
+                    event_meta[task][task] = {
+                        'onset': int(np.rint(story.iloc[0]['onset'] / tr)),
+                        'duration': int(np.rint(story.iloc[0]['duration'] / tr))}
+
+            # Check subsequent subjects for oddities
+            else:
+                if task != 'slumlordreach':
+                    new_onset = int(np.rint(story.iloc[0]['onset'] / tr))
+                    if event_meta[task][task]['onset'] != new_onset:
+                        print(f"{subject} has unexpected events: {story}"
+                              f"  expected {event_meta[task][task]}")
+
+    print(f"Finished compiling event onsets for {task}")
+
+with open(join(base_dir, 'code', 'event_meta.json'), 'w') as f:
+    json.dump(event_meta, f, indent=2, sort_keys=True)
 
 
 # Get some summary number of runs
